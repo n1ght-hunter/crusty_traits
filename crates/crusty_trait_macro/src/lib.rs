cb231d9290d4f8fe62e241b73b1e37749ac10cdf
Night_Hunter <samuelhuntnz@gmail.com>
2025-09-18 19:13:58 +1200

    vtable methods

--- crates/crusty_trait_macro/src/lib.rs
+++ crates/crusty_trait_macro/src/lib.rs
@@ -1,17 +1,13 @@
 //! macro to impl the crusty traits.
 
 use proc_macro2::TokenStream;
-use quote::{ToTokens, quote};
-use super_trait::{SuperTraits, get_super_traits};
-use syn::{
-    BareFnArg, Field, ItemStruct, LitStr, Token, TraitItem, Type, TypeBareFn, Visibility,
-    parse_quote, token::At,
-};
-use utils::{doc_attribute, repr_c_attribute};
+use super_trait::get_super_traits;
+use vtable::create_vtable;
 
 mod cdrop;
 mod super_trait;
 mod utils;
+mod vtable;
 
 pub(crate) const IGNORE_SUPER_TRAITS: [&str; 3] = ["Send", "Sync", "Sized"];
 
@@ -44,137 +40,15 @@ pub fn impl_crusty_trait(input: syn::ItemTrait) -> syn::File {
             .collect::<Vec<_>>();
 
     let cdrop_impl = cdrop::impl_cdrop_for_vtable(&vtable);
+    let vtable_methods = vtable::methods::impl_vtable_methods(&input, &vtable);
 
     output.items.push(input.into());
     output.items.push(vtable.into());
     output.items.extend(as_vtable_impls);
     output.items.push(cdrop_impl);
+    output.items.push(vtable_methods);
 
     output
-}
-
-fn create_vtable(
-    input: &syn::ItemTrait,
-    super_traits: &SuperTraits,
-) -> Result<ItemStruct, syn::Error> {
-    let trait_ident = &input.ident;
-    let repr_c = repr_c_attribute();
-    let string_ident = trait_ident.to_string();
-    let docs = doc_attribute(format!("A repr C vtable for the trait {}", string_ident));
-    let mut vtable = ItemStruct {
-        attrs: vec![repr_c, docs],
-        vis: input.vis.clone(),
-        struct_token: syn::token::Struct {
-            span: proc_macro2::Span::call_site(),
-        },
-        ident: utils::map_vtable_ident(trait_ident.clone()),
-        generics: input.generics.clone(),
-        fields: syn::Fields::Named(syn::FieldsNamed {
-            brace_token: syn::token::Brace::default(),
-            named: syn::punctuated::Punctuated::new(),
-        }),
-        semi_token: None,
-    };
-
-    let mut fields = input
-        .items
-        .iter()
-        .filter_map(|i| {
-            if let TraitItem::Fn(fn_item) = i {
-                Some(fn_item)
-            } else {
-                None
-            }
-        })
-        .map(|method| {
-            let ty = TypeBareFn {
-                lifetimes: None,
-                unsafety: Some(Default::default()),
-                abi: Some(syn::Abi {
-                    extern_token: Default::default(),
-                    name: Some(LitStr::new("C", proc_macro2::Span::call_site())),
-                }),
-                fn_token: Default::default(),
-                paren_token: Default::default(),
-                inputs: map_inputs(&method.sig.inputs, None).collect(),
-                variadic: None,
-                output: method.sig.output.clone(),
-            };
-            Field {
-                attrs: method.attrs.clone(),
-                vis: Visibility::Public(Default::default()),
-                mutability: syn::FieldMutability::None,
-                ident: Some(method.sig.ident.clone()),
-                colon_token: Some(Default::default()),
-                ty: Type::BareFn(ty),
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let super_trait_fields = super_traits
-        .iter()
-        .map(|super_trait| Field {
-            attrs: vec![],
-            vis: Visibility::Public(Default::default()),
-            mutability: syn::FieldMutability::None,
-            ident: Some(super_trait.field_ident.clone()),
-            colon_token: Some(Default::default()),
-            ty: super_trait.vtable_ty.clone(),
-        })
-        .collect::<Vec<_>>();
-
-    fields.extend(super_trait_fields);
-
-    let drop_field: Field = parse_quote!(
-        #[doc = "A function pointer to the drop function for the trait"]
-        pub drop: unsafe extern "C" fn(CRefMut<Self>)
-    );
-    fields.push(drop_field);
-
-    vtable.fields = syn::Fields::Named(syn::FieldsNamed {
-        brace_token: syn::token::Brace::default(),
-        named: fields.into_iter().collect(),
-    });
-
-    Ok(vtable)
-}
-
-fn map_inputs(
-    inputs: &syn::punctuated::Punctuated<syn::FnArg, Token![,]>,
-    name: Option<TokenStream>,
-) -> impl Iterator<Item = BareFnArg> {
-    let name = name.unwrap_or_else(|| quote! { Self });
-    inputs.iter().map(move |arg: &syn::FnArg| match arg {
-        syn::FnArg::Receiver(recv) => {
-            let ty = recv.ty.as_ref().clone();
-
-            match ty {
-                Type::Reference(type_ref) if type_ref.mutability.is_none() => BareFnArg {
-                    attrs: recv.attrs.clone(),
-                    name: None,
-                    ty: parse_quote!(CRef<#name>),
-                },
-                Type::Reference(_) => BareFnArg {
-                    attrs: recv.attrs.clone(),
-                    name: None,
-                    ty: parse_quote!(CRefMut<#name>),
-                },
-                Type::Path(_) => BareFnArg {
-                    attrs: recv.attrs.clone(),
-                    name: None,
-                    ty: parse_quote!(CRepr<#name>),
-                },
-                _ => {
-                    panic!("Receiver type must be a reference");
-                }
-            }
-        }
-        syn::FnArg::Typed(pat_type) => BareFnArg {
-            attrs: pat_type.attrs.clone(),
-            name: None,
-            ty: pat_type.ty.as_ref().clone(),
-        },
-    })
 }
 
 #[cfg(test)]
